ec585f217af7ca55292b0941836256298a623d2e
John Williams <smailliw@gmail.com>
2011-05-04 13:30:58 -0600

    better optimized pdfs and descriptive names

--- LDSHymns/downloads/makexml.pl
+++ LDSHymns/downloads/makexml.pl
@@ -4,13 +4,18 @@ open(OUT,'>','ldshymns.download.xml') or die $!;
 
 print OUT <<END;
 <?xml version="1.0" encoding="utf-8"?>
-<config version="1.1">
+<config version="1.2">
 END
 
 for my $f (sort glob "*.pdf") {
 my $size = -s $f;
-my $md5 = `md5sum $f`;
+my $md5 = `md5sum "$f"`;
 $md5 =~ s/\s.*$//s;
+$f =~ s/</&lt;/g;
+$f =~ s/>/&gt;/g;
+$f =~ s/"/&quot;/g;
+$f =~ s/'/&apos;/g;
+$f =~ s/&/&amp;/g;
 print OUT <<END;
   <file src="$f" dest="$f" size="$size" md5="$md5" />
 END
